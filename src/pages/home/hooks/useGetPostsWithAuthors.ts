f761698bf69b91f990d1b74c667a3eeffdfb4a1f
Bartek <depciuch322@gmail.com>
2024-07-19 02:22:46 +0200

    Add useGetPostsWithAuthors hook to fetch and combine posts with author names

---
+++ src/pages/home/hooks/useGetPostsWithAuthors.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { ModelPostWithAuthor, ModelUser } from '../types/modelHome';
+import { getPosts } from '../services/postsService';
+import { getUsers } from '../services/usersService';
+
+const useGetPostsWithAuthors = () => {
+  const [postsData, setPostsData] = useState<ModelPostWithAuthor[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchPostsAndUsers = async () => {
+    try {
+      const [posts, users] = await Promise.all([getPosts(), getUsers()]);
+
+      const userMap = new Map<number, string>();
+      users.forEach((user: ModelUser) => {
+        userMap.set(user.id, user.name);
+      });
+
+      const postsWithAuthors = posts.map((post) => ({
+        ...post,
+        author: userMap.get(post.userId) || 'Unknown',
+      }));
+
+      setPostsData(postsWithAuthors);
+    } catch (error) {
+      setError('Error fetching posts or users');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostsAndUsers();
+  }, []);
+
+  return {
+    postsData,
+    loading,
+    error,
+  };
+};
+
+export default useGetPostsWithAuthors;
