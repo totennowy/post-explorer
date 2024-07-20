7ffc028ddd9becad330630aab77fab6b36b0c71b
Bartek <depciuch322@gmail.com>
2024-07-20 23:47:59 +0200

    Create HomeTableRow component

---
+++ src/pages/home/components/HomeTableRow.tsx
@@ -0,0 +1,20 @@
+import { ModelHomeTableRow } from "../types/modelHomeTableRow";
+
+
+const HomeTableRow: React.FC<ModelHomeTableRow> = ({
+  author,
+  id,
+  title,
+  body,
+}) => {
+  return (
+    <tr>
+      <td>{author}</td>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{body}</td>
+    </tr>
+  );
+};
+
+export default HomeTableRow;
---
+++ src/pages/home/types/modelHomeTableRow.ts
@@ -0,0 +1,6 @@
+export type ModelHomeTableRow = {
+  author: string;
+  id: number;
+  title: string;
+  body: string;
+};
