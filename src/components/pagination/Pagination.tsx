bcc59ed85df67d5153898ca791ffbfc1bcef3fbd
Bartek <depciuch322@gmail.com>
2024-07-22 22:49:12 +0200

    Add Pagination component

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,24 @@
+import { ModelPagination } from './types/modelPagination';
+
+const Pagination: React.FC<ModelPagination> = ({
+  rowsPerPage,
+  setRowsPerPage,
+}) => {
+  return (
+    <div>
+      <label htmlFor="rowsPerPage">Rows per page: </label>
+      <select
+        id="rowsPerPage"
+        value={rowsPerPage}
+        onChange={(e) => setRowsPerPage(Number(e.target.value))}
+      >
+        <option value={10}>10</option>
+        <option value={20}>20</option>
+        <option value={50}>50</option>
+        <option value={-1}>All</option>
+      </select>
+    </div>
+  );
+};
+
+export default Pagination;
