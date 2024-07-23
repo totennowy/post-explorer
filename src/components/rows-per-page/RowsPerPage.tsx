47d352155327eb540f600a43b37c097bbe2f3b06
Bartek <depciuch322@gmail.com>
2024-07-23 23:54:39 +0200

    Add handleChange logic inside RowsPerPage component

--- src/components/rows-per-page/RowsPerPage.tsx
+++ src/components/rows-per-page/RowsPerPage.tsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import { ModelRowsPerPage } from './types/modelRowsPerPage';
 
 const RowsPerPage: React.FC<ModelRowsPerPage> = ({
   rowsPerPage,
   setRowsPerPage,
+  resetCurrentPage,
 }) => {
+  
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRowsPerPage(Number(e.target.value));
+    resetCurrentPage();
+  };
+
   return (
     <div className="pagination_container">
       <label htmlFor="rowsPerPage">Rows per page: </label>
-      <select
-        id="rowsPerPage"
-        value={rowsPerPage}
-        onChange={(e) => setRowsPerPage(Number(e.target.value))}
-      >
+      <select id="rowsPerPage" value={rowsPerPage} onChange={handleChange}>
         <option value={10}>10</option>
         <option value={20}>20</option>
         <option value={50}>50</option>
-        <option value={-1}>All</option>
       </select>
     </div>
   );
