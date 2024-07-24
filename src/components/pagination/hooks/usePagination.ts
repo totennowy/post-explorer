ef3eb484ef16f54d08cf2ecb37fa3ae65db14b0d
Bartek <depciuch322@gmail.com>
2024-07-24 22:30:35 +0200

    Improve usePagination hook for better page handling

--- src/components/pagination/hooks/usePagination.ts
+++ src/components/pagination/hooks/usePagination.ts
@@ -1,26 +1,58 @@
-import { useState } from 'react';
+import { useMemo } from 'react';
+import { ModelUsePaginationProps } from '../types/modelPagination';
 
-const usePagination = (totalItems: number, rowsPerPage: number) => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+export const DOTS = '...';
 
-  const totalPages = Math.ceil(totalItems / rowsPerPage);
-
-  const handlePageChange = (newPage: number) => {
-    if (newPage > 0 && newPage <= totalPages) {
-      setCurrentPage(newPage);
-    }
-  };
-
-  const resetCurrentPage = () => {
-    setCurrentPage(1);
-  };
-
-  return {
-    currentPage,
-    totalPages,
-    handlePageChange,
-    resetCurrentPage,
-  };
+const range = (start: number, end: number) => {
+  return Array.from({ length: end - start + 1 }, (_, idx) => idx + start);
 };
 
-export default usePagination;
+export const usePagination = ({
+  totalCount,
+  pageSize,
+  siblingCount = 1,
+  currentPage,
+}: ModelUsePaginationProps): (number | string)[] => {
+  const paginationRange = useMemo(() => {
+    const totalPageCount = Math.ceil(totalCount / pageSize);
+    const totalPageNumbers = siblingCount + 5;
+
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount,
+    );
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      return [...range(1, 3 + 2 * siblingCount), DOTS, totalPageCount];
+    }
+
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      return [
+        1,
+        DOTS,
+        ...range(totalPageCount - (3 + 2 * siblingCount) + 1, totalPageCount),
+      ];
+    }
+
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      return [
+        1,
+        DOTS,
+        ...range(leftSiblingIndex, rightSiblingIndex),
+        DOTS,
+        totalPageCount,
+      ];
+    }
+
+    return [];
+  }, [totalCount, pageSize, siblingCount, currentPage]);
+
+  return paginationRange;
+};
