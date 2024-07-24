8ca0d45a84e562634940d34ceea76bba98505097
Bartek <depciuch322@gmail.com>
2024-07-24 22:30:24 +0200

    Improve pagination component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,37 +1,72 @@
 import React from 'react';
+
 import { ModelPagination } from './types/modelPagination';
+import { DOTS, usePagination } from './hooks/usePagination';
 
 const Pagination: React.FC<ModelPagination> = ({
   currentPage,
-  totalPages,
-  handlePageChange,
+  totalCount,
+  siblingCount = 1,
+  pageSize,
+  onPageChange,
 }) => {
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    if (currentPage < Math.ceil(totalCount / pageSize)) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const onPrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
 
   return (
-    <div className="pagination_container">
-      <button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 1}
+    <ul className="pagination-container">
+      <li
+        className={`pagination-item ${currentPage === 1 ? 'disabled' : ''}`}
+        onClick={onPrevious}
       >
         &lt;
-      </button>
-      {pages.map((page) => (
-        <button
-          key={page}
-          onClick={() => handlePageChange(page)}
-          className={page === currentPage ? 'active' : ''}
-        >
-          {page}
-        </button>
-      ))}
-      <button
-        onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
+      </li>
+      {paginationRange.map((pageNumber, index) =>
+        pageNumber === DOTS ? (
+          <li key={index} className="pagination-item dots">
+            &#8230;
+          </li>
+        ) : (
+          <li
+            key={index}
+            className={`pagination-item ${
+              pageNumber === currentPage ? 'selected' : ''
+            }`}
+            onClick={() => onPageChange(pageNumber as number)}
+          >
+            {pageNumber}
+          </li>
+        ),
+      )}
+      <li
+        className={`pagination-item ${
+          currentPage === Math.ceil(totalCount / pageSize) ? 'disabled' : ''
+        }`}
+        onClick={onNext}
       >
         &gt;
-      </button>
-    </div>
+      </li>
+    </ul>
   );
 };
 
