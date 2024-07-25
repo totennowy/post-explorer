ad89bcd1e12f4af43c99e48c92768bda5e8d3ef8
Bartek <depciuch322@gmail.com>
2024-07-25 16:45:03 +0200

    Add TableFilter component

---
+++ src/components/table-filter/TableFilter.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import useTableFilter from './hooks/useTableFilter';
+import { ModelTableFilter } from './types/modelTableFilter';
+import arrowFilterIcon from '@assets/top-bottom-arrow.svg';
+
+const TableFilter: React.FC<ModelTableFilter> = ({
+  options,
+  onChange,
+  placeholder,
+}) => {
+  const {
+    selectedOptions,
+    isOpen,
+    toggleFilter,
+    filterRef,
+    handleCheckboxChange,
+  } = useTableFilter(onChange);
+
+  return (
+    <div className="table_filter_container" ref={filterRef}>
+      <button className="filter_button" onClick={toggleFilter}>
+        <img src={arrowFilterIcon} alt="filter icon" className="filter_icon" />
+      </button>
+      {isOpen && (
+        <div className="table_filter_content">
+          <span>{placeholder}</span>
+          <div className="checkbox_options">
+            {options.map((option) => (
+              <label key={option.value}>
+                <input
+                  type="checkbox"
+                  value={option.value}
+                  checked={selectedOptions.includes(option.value)}
+                  onChange={() => handleCheckboxChange(option.value)}
+                />
+                {option.label}
+              </label>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TableFilter;
